89b7f2d7e6321163a700b5f9c1e9b4b8a6c91e01
fikri <fikrifikri934@gmail.com>
2025-06-28 18:18:03 +0800

    delete uploadthing

--- app/api/uploadthing/route.ts
+++
@@ -1,8 +0,0 @@
-// app/api/uploadthing/route.ts
-import { createRouteHandler as createNextRouteHandler } from "uploadthing/next";
-import { ourFileRouter } from "./core";
-
-// Ekspor handler route untuk app router
-export const { GET, POST } = createNextRouteHandler({
-  router: ourFileRouter,
-});
